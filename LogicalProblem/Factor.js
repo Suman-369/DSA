f616812a023f0cb173ee800cf0de32974beff591
Suman_Kar <www.skar2022@gmail.com>
2025-06-28 10:59:22 +0530

    Update Factor.js

--- LogicalProblem/Factor.js
+++ LogicalProblem/Factor.js
@@ -8,6 +8,7 @@ let output = []
 for(let i = 1 ; i<=n ;i++){
     if(n%i === 0){
         output.push(i)
+    // you can write this also output += i + " " 
     }
 
     
@@ -15,3 +16,7 @@ for(let i = 1 ; i<=n ;i++){
 
 
     console.log(output.join(" "));
+// write also cnsole.log(output.trim())
+
+
+//  THE OUTPUT IS => If the input is 6 then output 1 2 3 6 

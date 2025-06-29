dd6e941ad12b7c0a0b27d57288fe2c0ed8b1c2e9
Suman_Kar <www.skar2022@gmail.com>
2025-06-29 12:09:04 +0530

    Update Max elem from array.js

--- Arrays/Max elem from array.js
+++ Arrays/Max elem from array.js
@@ -1,2 +1,13 @@
 // let prompt = require("prompt-sync")();
 
+let arr = [10 , 5 , 15 , 8 , 3, 46 ,19]
+
+let max = arr[0]
+for(let i =1 ; i<arr.length;i++){
+   if(arr[i]>max){
+     max = arr[i]
+   }
+}
+
+console.log("Max Element => " + max);
+

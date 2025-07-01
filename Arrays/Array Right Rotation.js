a7a0c6c2c24aa4f64a84333249a5b6124e46fff8
suman kar <enter>
2025-07-01 14:24:22 +0530

    update

---
+++ Arrays/Array Right Rotation.js
@@ -0,0 +1,11 @@
+let arr = [10, 5, 15, 8, 3, 46, 19];
+
+let first = arr[arr.length - 1];
+
+for(let i = arr.length - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+}
+
+arr[0] = first;
+
+console.log(arr);

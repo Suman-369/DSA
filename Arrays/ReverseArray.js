498b6af19a0a594a6e38b49a74401be2c20ca2ef
suman kar <enter>
2025-07-01 13:51:25 +0530

    update

--- Arrays/ReverseArray.js
+++ Arrays/ReverseArray.js
@@ -13,3 +13,16 @@ console.log(arr);
 
 
 // it will bw a any other approach 
+
+// let arr = [10, 5, 15, 8, 3, 46, 19];
+
+// let temp = new Array(arr.length);
+
+// let i= 0 
+
+// for(let j = arr.length -1 ; j>=0 ; j--){
+//     temp[i] = arr[j]
+//     i++
+// }
+
+// console.log(temp);

8a5416ac4701b909d9acbc7f288a4fd4b9717871
suman kar <enter>
2025-07-01 13:42:48 +0530

    reverse array

---
+++ Arrays/ReverseArray.js
@@ -0,0 +1,12 @@
+let arr = [10, 5, 15, 8, 3, 46, 19];
+
+let left = 0 ,right = arr.length-1
+while(left<right){
+    let temp = arr[left]
+    arr[left] = arr[right]
+    arr[right] = temp
+    left++
+    right--
+}
+
+console.log(arr);

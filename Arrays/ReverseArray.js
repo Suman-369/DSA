b5e7987c0c25f6bfdbc98d4290cf1c31a0f2b6d8
suman kar <enter>
2025-07-03 13:34:19 +0530

    update

--- Arrays/ReverseArray.js
+++ Arrays/ReverseArray.js
@@ -1,28 +1,28 @@
-let arr = [10, 5, 15, 8, 3, 46, 19];
+// let arr = [10, 5, 15, 8, 3, 46, 19];
 
-let left = 0 ,right = arr.length-1
-while(left<right){
-    let temp = arr[left]
-    arr[left] = arr[right]
-    arr[right] = temp
-    left++
-    right--
-}
+// let left = 0 ,right = arr.length-1
+// while(left<right){
+//     let temp = arr[left]
+//     arr[left] = arr[right]
+//     arr[right] = temp
+//     left++
+//     right--
+// }
 
-console.log(arr);
+// console.log(arr);
 
 
 // it will bw a any other approach 
 
-// let arr = [10, 5, 15, 8, 3, 46, 19];
+let arr = [10, 5, 15, 8, 3, 46, 19];
 
-// let temp = new Array(arr.length);
+let temp = new Array(arr.length);
 
-// let i= 0 
+let i= 0 
 
-// for(let j = arr.length -1 ; j>=0 ; j--){
-//     temp[i] = arr[j]
-//     i++
-// }
+for(let j = arr.length -1 ; j>=0 ; j--){
+    temp[i] = arr[j]
+    i++
+}
 
-// console.log(temp);
+console.log(temp);
---
+++ Arrays/rotation.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5]
+
+console.log(arr);
+
+let k = 5
+
+k = k%arr.length
+
+for(let j = 1 ; j<=k ; j++){
+    let copy = arr[0]
+    for(let i = 0; i<arr.length-1; i++){
+        arr[i] = arr[i+1]
+    }
+    arr[arr.length-1] = copy
+}
+
+console.log(arr);

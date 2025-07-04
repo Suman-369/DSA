58a8c8a103fb67d7e87a9c6f508c9bc4bf9c02e0
suman kar <enter>
2025-07-04 13:37:12 +0530

    bobble Sort

---
+++ Sorting/bobblesort.js
@@ -0,0 +1,16 @@
+let arr = [5, 3, 8, 4, 2, 6];
+
+for(let i =0 ; i<arr.length-1;i++){
+    for(let j = 0 ; j < arr.length - i - 1; j++){
+        if(arr[j]>arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }else{
+            console.log("Already Sorted");
+            
+        }
+    }
+}
+
+console.log(arr);

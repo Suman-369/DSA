2f95136ed8585cf6a054cc84e01fcae951e47318
suman kar <enter>
2025-07-04 13:40:46 +0530

    update

--- Sorting/bobblesort.js
+++ Sorting/bobblesort.js
@@ -1,20 +1,17 @@
 let arr = [5, 3, 8, 4, 2, 6];
 
-for(let i =0 ; i<arr.length-1;i++){
-    for(let j = 0 ; j < arr.length - i - 1; j++){
-        if(arr[j]>arr[j+1]){
-            let temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-        }
+for (let i = 0; i < arr.length - 1; i++) {
+  for (let j = 0; j < arr.length - i - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
     }
+  }
 }
 
 console.log(arr);
 
-
-//OUT PUT 
-
+//OUT PUT
 
 // [ 2, 3, 4, 5, 6, 8 ]
-

deb3ce35f0da2ab1d985172ec99df99bfa19f3a9
suman kar <enter>
2025-07-04 13:52:58 +0530

    Selection Sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,18 @@
+
+let arr= [5, 3, 8, 4, 2, 6];
+
+for(let i = 0 ; i<arr.length;i++){
+    let small = i;
+    for(let  j= i+1 ; j<arr.length;j++){
+        if(arr[small]> arr[j]){
+            small = j
+        }
+    }
+    if(small != i){
+        let temp = arr[i];
+        arr[i] = arr[small];
+        arr[small] = temp;
+    }
+}
+
+console.log(arr);

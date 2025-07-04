eca3917e27cc625ecde0538cc1ba878281d84c7c
suman kar <enter>
2025-07-04 14:04:34 +0530

    Insertion Sost

---
+++ Sorting/InsertionSort.js
@@ -0,0 +1,14 @@
+let arr = [5, 3, 8, 4, 2, 6];
+
+for (let i = 0; i < arr.length ; i++) {
+    let key = arr[i];
+
+    let j = i-1;
+
+    while(j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j --;
+    }
+    arr[j + 1] = key;
+}
+console.log(arr);

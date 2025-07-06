d4dc1125afafdd01dc13f01f010f59d33ee164af
suman kar <enter>
2025-07-06 16:07:24 +0530

    update

---
+++ MultyDimensionalArrays/Sum of array.js
@@ -0,0 +1,17 @@
+let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+let leftSum = 0 , rightSum = 0 ;
+
+for(let i = 0 ; i < arr.length ; i++){
+    for(let  j = 0 ; j< arr[i].length ; j++){
+        if(i ==j){
+            leftSum += arr[i][j];  
+        }
+        if(i+j == arr.length-1){
+            rightSum += arr[i][j];  
+        }
+    }
+}
+
+console.log("Left Diagonal Sum: " + leftSum);
+console.log("Right Diagonal Sum: " + rightSum);

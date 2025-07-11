c74d9660947744a7c4e7f207d39a83371a60cd81
Suman_Kar <www.skar2022@gmail.com>
2025-07-11 16:50:09 +0530

    Update SpiralMatrix.js

--- MultyDimensionalArrays/SpiralMatrix.js
+++ MultyDimensionalArrays/SpiralMatrix.js
@@ -20,18 +20,19 @@ let left = 0
 let right = matrix[0].length-1
 
 while(top <= bottom && left<=right){
-
+   // Traverse top row
     for(let i = left ; i<=right;i++){
         result.push(matrix[top][i])
     }
     top++
-
+    // Traverse right column
     for(let i =top ;i<=bottom;i++){
         result.push(matrix[i][right])
 
     }
     right--
 
+     // Traverse bottom row
     if(top<=bottom){
         for(let i = right ; i>=left;i--){
             result.push(matrix[bottom][i])
@@ -39,6 +40,7 @@ while(top <= bottom && left<=right){
         bottom--
     }
 
+     // Traverse left column
     if(left <= right){
         for(let i = bottom ; i>=top;i--){
             result.push(matrix[i][left])

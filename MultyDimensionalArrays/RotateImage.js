3d8c6f597fbfe020c2603cbf1203ddf2d0fc2abc
Suman_Kar <www.skar2022@gmail.com>
2025-07-11 12:58:30 +0530

    Update RotateImage.js

--- MultyDimensionalArrays/RotateImage.js
+++ MultyDimensionalArrays/RotateImage.js
@@ -1,5 +1,8 @@
 // Its A leetcode problem  question no 48 Rorate Image  ** constraints are DO NOT alocate extra array to rotation 
 
+// The Soln :   First of all Rotate = Transpose + revarse   ** means we perform a two operation for this problem firts of the transpose here we do the swap between the row ans column element
+//              then reverse the row element 
+
 
 let arr = [[1,2,3],[4,5,6],[7,8,9]];
 

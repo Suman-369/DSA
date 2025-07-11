1eee78c34e510f6ec8e516aea160dd80b7f960f9
suman kar <enter>
2025-07-11 12:53:16 +0530

    update

--- Arrays/tempCodeRunnerFile.js
+++ Arrays/tempCodeRunnerFile.js
@@ -1,12 +1,12 @@
 let arr = [10, 5, 15, 8, 3, 46, 19];
 
-let temp = new Array(arr.length);
-
-let i= 0 
-
-for(let j = arr.length -1 ; j>=0 ; j--){
-    temp[i] = arr[j]
-    i++
+let left = 0 ,right = arr.length-1
+while(left<right){
+    let temp = arr[left]
+    arr[left] = arr[right]
+    arr[right] = temp
+    left++
+    right--
 }
 
-console.log(temp);
+console.log(arr);
---
+++ MultyDimensionalArrays/RotateImage.js
@@ -0,0 +1,26 @@
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+
+for(let i = 0 ; i< arr.length ; i++){
+    for(let j = i ; j<arr[i].length ; j++){
+        // Swap the elements
+        let temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+        
+    }
+}
+
+for(let i = 0 ; i <arr.length ; i++){
+let k = 0 ;j = arr.length -1;
+while(k<j){
+   let temp  =  arr[i][k]
+    arr[i][k] = arr[i][j]
+    arr[i][j] = temp
+    k++;    
+    j--;
+}
+}
+
+
+console.log(arr);

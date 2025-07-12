ca132f255273f8abff899c1671cda00f6cb64584
suman kar <enter>
2025-07-12 11:55:45 +0530

    update

---
+++ Strings/Q1.js
@@ -0,0 +1,6 @@
+let s = "suman"
+
+for(let i = 0 ; i<s.length;i++){
+    console.log(s[i]);
+    
+}
---
+++ Strings/Q2.js
@@ -0,0 +1,9 @@
+let s = "Suman"
+
+let rev = ""
+
+for(let i = s.length-1;i>=0;i--){
+    rev = rev + s.charAt(i)
+}
+
+console.log(rev);

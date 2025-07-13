dff24b3a6c08daa5fcc5e1061a2ad311de4202f9
suman kar <enter>
2025-07-13 10:33:28 +0530

    Update

---
+++ Strings/palindromString.js
@@ -0,0 +1,20 @@
+let prompt = require("prompt-sync")()
+
+let s =  prompt("enter a string : ")
+
+let isPallindrom = true 
+
+let i = 0 , j = s.length-1
+
+while(i<j){
+    if(s[i] != s[j]){
+        isPallindrom = false
+        break
+    }
+    i++
+    j--
+}
+
+if(isPallindrom) console.log("Its A Pallindrom ");
+
+else console.log("Its Not A Pallindrom ");

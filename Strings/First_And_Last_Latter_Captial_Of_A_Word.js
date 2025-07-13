11d3dd60e3b1ca0933ba4d294d7f7850858aa4ac
suman kar <enter>
2025-07-13 11:48:58 +0530

    update

---
+++ Strings/First_And_Last_Latter_Captial_Of_A_Word.js
@@ -0,0 +1,15 @@
+let s = "hello suman kya haal chaal"
+
+
+let ans = ""
+
+let arrStr = s.split(" ")
+for(let i = 0 ; i<arrStr.length;i++){
+    let word = arrStr[i]
+    if(word.length<=2) ans +=word.toUpperCase() + " "
+        else{
+           ans += word.charAt(0).toUpperCase() + word.substring(1,word.length-1) + word.charAt(word.length-1).toUpperCase() +" "
+    }
+}
+
+console.log(ans);

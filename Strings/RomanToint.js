b121389e2dddcb3da96cdf17fb610c3da8217bf2
suman kar <enter>
2025-07-13 12:27:14 +0530

    roman to int

---
+++ Strings/RomanToint.js
@@ -0,0 +1,32 @@
+let prompt = require("prompt-sync")()
+
+let s = prompt("Enter A Roman No : ").toUpperCase().trim();
+
+
+let roman = {
+    'I' :1,
+    'V' : 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+let result = 0
+
+for(let i = 0 ; i<s.length;i++){
+    let currentval = roman[s[i]]
+    let nextval = roman[s[i+1]]
+
+    if(nextval>currentval){
+        result += nextval - currentval
+        i++
+    }
+    else{
+        result += currentval
+    }
+
+}
+
+console.log(result);

65d566e91228045c66bb36bfd43eb9cf78abf33c
suman kar <enter>
2025-07-14 13:35:30 +0530

    update pangram

---
+++ Strings/pangram.js
@@ -0,0 +1,17 @@
+let sentence = 'abcdefghijklmnopqrstuvwxyz'
+
+let set = new Set()
+
+for(let i = 0 ; i<sentence.length;i++){
+    let ch = sentence.charAt(i)
+    set.add(ch)
+}
+
+if(set.size==26){
+    console.log(true);
+    
+}
+else{
+    console.log(false);
+    
+}
---
+++ Strings/set.js
@@ -0,0 +1,9 @@
+// set.add()
+// set.delete()
+// set.has()
+// set.size()
+
+//travarse
+// for(let a of set){
+//     process.stdout.write(a+" ")
+// }

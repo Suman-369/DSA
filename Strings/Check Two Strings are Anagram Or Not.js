6a3bea403c054d6771971eee5d4eaf10b8652748
suman kar <enter>
2025-07-14 13:09:43 +0530

    update

---
+++ Strings/Check Two Strings are Anagram Or Not.js
@@ -0,0 +1,30 @@
+let prompt = require("prompt-sync")()
+
+let s1 = prompt ("Enter String One :")
+let s2 = prompt ("Enter String Two :")
+
+let freqArr = new Array(123).fill(0)
+if(s1.length!= s2.length){
+    console.log("Not Anagram");
+    
+}
+else{
+    let isAnagram = true
+for(let i = 0 ; i<s1.length;i++){
+    let asci = s1.charCodeAt(i)
+    freqArr[asci] = freqArr[asci]+1
+}
+for(let i = 0 ; i<s2.length;i++){
+    let asci = s2.charCodeAt(i)
+    freqArr[asci] = freqArr[asci]-1
+}
+for(let i = 0 ;i<freqArr.length;i++){
+    if(freqArr[i]!=0){
+        isAnagram = false
+        break
+    }
+}
+ if(isAnagram) console.log("Given String are Anagram");
+ else console.log("Given String are Not Anagram");
+ 
+}

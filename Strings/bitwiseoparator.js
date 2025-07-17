0b4b0030e994d3a541a1f4a2839c27c3fd781e96
Suman_Kar <www.skar2022@gmail.com>
2025-07-17 23:30:23 +0530

    Update bitwiseoparator.js

--- Strings/bitwiseoparator.js
+++ Strings/bitwiseoparator.js
@@ -2,12 +2,12 @@
 //right shift operator
 // here firstof all wi divide the 25 into its binary number and then right shift with 2 
 
-console.log(25>>2);
+console.log(25>>2); // out put 6 
 
 //Left shift operator 
 // here first we divide the 25 into his binary number and then left shift by 2 and then the right shide make 2 empty box so we put two zero over there and now calculate the number we get te value 
 
-console.log(25<<2);
+console.log(25<<2); // out put 100
 
 
 

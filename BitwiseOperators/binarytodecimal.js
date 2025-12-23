1210c4dc8c4187db2fa39ea60315154e08e79629
suman kar <enter>
2025-12-23 12:43:35 +0530

    question 1

--- BitwiseOperators/binarytodecimal.js
+++ BitwiseOperators/binarytodecimal.js
@@ -17,3 +17,17 @@ console.log(25>>2);
 console.log(25<<2);
 
 // Out put = > 100
+
+
+let count = 0 , num =64
+
+while(num !=0){
+    if((num & 1) == 1){
+    break
+}else{
+    count = count+1
+    num = num >> 1
+} 
+}
+
+console.log(count);

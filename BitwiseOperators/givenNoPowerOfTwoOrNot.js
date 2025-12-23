7b4f9e1e0776ae9509ec6b1d5d82e28847a07727
Suman_Kar <www.skar2022@gmail.com>
2025-12-23 14:21:20 +0530

    Update givenNoPowerOfTwoOrNot.js

--- BitwiseOperators/givenNoPowerOfTwoOrNot.js
+++ BitwiseOperators/givenNoPowerOfTwoOrNot.js
@@ -1,4 +1,4 @@
-// 2 -> 10
+// 2 -> 10  || MSB is carry 1 and other Bits is all 0 this binary operation is called the powr of 2 
 // 4 -> 100    
 // 8 -> 1000
 
@@ -7,11 +7,13 @@
 // 64 -> 1000000
 
 
-// 1 -> 1
+// 1 -> 1  || all the number which is the power of two and the prev number of that number always carry all the 1
 // 3 -> 11
 // 7 -> 111
 // 15 -> 1111
 
+
+// its a check if the given number is power of two or not 
 
 let prompt = require("prompt-sync")()
 

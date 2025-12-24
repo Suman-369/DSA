d6c168ab35c82b99232289e00c0cc37eea8fdbf7
Suman_Kar <www.skar2022@gmail.com>
2025-12-24 11:44:13 +0530

    Update MathProblems.js

--- Recursion/MathProblems.js
+++ Recursion/MathProblems.js
@@ -15,13 +15,14 @@ for (let i = Math.min(a, b); i >= 1; i--) {
 
 // recursive function to find HCF/GCD of two numbers
 // with the Euclidian algorithm
+// Euclidian formula humlogoko bolta hai ki substract karo bade value se chota value  and jo reminder ata hai usko store ka do bade wale value ke jagaha 
 
-let c = 16;
-let d = 24;
+let c = 16; 
+let d = 24;  
+while (c != d) { // check c or d same or not 
+  if (c > d) c = c - d; // if c is big the d or not 
 
-while (c != d) {
-  if (c > d) c = c - d;
-  else d = d - c;
+  else d = d - c; // else then // 24 - 26 = 8  => and store the 8 at place of 24 
 }
 
-console.log(c);
+console.log(c);// print the any of the value c or d 

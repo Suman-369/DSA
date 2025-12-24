c6d95fcf774c16137a8a47216df42b9e61deb708
Suman_Kar <www.skar2022@gmail.com>
2025-12-24 11:08:17 +0530

    Update reverseofanumber.js

--- Recursion/reverseofanumber.js
+++ Recursion/reverseofanumber.js
@@ -1,16 +1,16 @@
 // NOrmal Way to solve reverse of a digit 
 
-let n = 369;
+let n = 369; // initialize the number 
 
-let rev = 0;
+let rev = 0; // set the reverse value initially 0 
 
-while (n > 0) {
-  let rem = n % 10;
-  rev = rev * 10 + rem;
-  n = Math.floor(n / 10);
+while (n > 0) {  // check if the number getter then 0 or not 
+  let rem = n % 10;  // yes -> reminder nikalo means find the last digit  => 9 
+  rev = rev * 10 + rem;  //  (0*10) + 9  => 9  calculated value set on the rev , now the value of rev is 9 
+  n = Math.floor(n / 10);  // ek number ko kam karke fhir bhejo , means cut the one number from the digit now go => 36
 }
 
-console.log("Reversed Number is : ", rev);
+console.log("Reversed Number is : ", rev); // print the last  rev value 
 
 
 

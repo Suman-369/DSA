34c452b25aaf1350d1fe045038233332f18427e0
suman kar <enter>
2025-12-24 11:22:44 +0530

    highest common factor

---
+++ Recursion/MathProblems.js
@@ -0,0 +1,13 @@
+// Highest Common Factor (HCF) or Greatest Common Divisor (GCD) of two numbers using iteration
+
+
+let a = 32
+
+let b = 20
+
+for ( let i = Math.min(a,b) ; i>=1 ; i-- ){
+    if( a % i === 0 && b % i === 0 ){
+        console.log(i)
+        break
+    }
+}

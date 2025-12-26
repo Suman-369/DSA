491c7e063af513643bd79e5cff01560d2f965da1
suman kar <enter>
2025-12-26 13:02:51 +0530

    leet code 169 majority element

---
+++ AdvanceedArray/majorityElement.js
@@ -0,0 +1,26 @@
+function majorityElem(nums){
+
+    let count = 1 ;
+    let ans = nums[0]
+
+    for(let i = 1 ; i<nums.length; i++){
+        if(count ==0){
+            ans = nums[i]
+            count = 1
+        }
+        else if( ans != nums[i]){
+            count -- ;
+
+        }
+        else {
+            count ++
+        }
+    }
+return ans
+
+}
+
+
+
+let nums = [3,2,3];
+console.log(majorityElem(nums));

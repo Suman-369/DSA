70bf57a69000b64ee95523f1cd0b41773048be7e
Suman_Kar <www.skar2022@gmail.com>
2025-12-26 13:44:38 +0530

    Update maxProfit.js

--- AdvanceedArray/maxProfit.js
+++ AdvanceedArray/maxProfit.js
@@ -1,7 +1,14 @@
 // leetcode 121 question Max profit 
 
 
-// QUestion ka motive a hai ki tumko sabse chota stock bue karna padega in an array and feuture main max profit k sath bechna padega 
+// QUestion ka motive a hai ki tumko sabse chota stock bue karna padega in an array and feuture main max profit k sath bechna padega  ** past main nahi bechna always future main bechna hai 
+
+// solution : => array ka first index ko  min dharlo and max profit ko 0 rakkho 
+// then ek loop lo jo ki 0 se mera array ka length tak jayega 
+// then ceec karo min ka value bada hai kay  current index ka value se agar hai to min ka value change karke index ka value kardo 
+// then prfit calculate karo jo ki index - min hai 
+// then check  karo max profit or current profit konsa bada hai 
+// bas last main maxProfit ko return kardo 
 
 function maxProfit(nums){
     let min = nums[0]; // sabse pehla value ko min main store karlo

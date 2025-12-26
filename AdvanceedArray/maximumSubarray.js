eff60fa0fa30fb353fe7ca6c85fdd66119018a7d
Suman_Kar <www.skar2022@gmail.com>
2025-12-26 10:19:49 +0530

    Update maximumSubarray.js

--- AdvanceedArray/maximumSubarray.js
+++ AdvanceedArray/maximumSubarray.js
@@ -1,22 +1,24 @@
-function maxSubArray( nums){
-    let sum = nums[0];
-    let maxSum = nums[0];
+// kadans Algo
 
-    for(let i =0 ; i<nums.length; i++){
-       if(sum<0){
-        sum = nums[i]
+function maxSubArray( nums){
+    let sum = nums[0]; // initialize sum with first index with value 0 
+    let maxSum = nums[0]; // initialize maxSum with first index with value 0 
+
+    for(let i =0 ; i<nums.length; i++){ // loop upto 1 to length
+       if(sum<0){  // if sum  - ve then make the sum 0 
+        sum = nums[i] // -ve then stay as it is 
        } 
        else{
-        sum += nums[i]
+        sum += nums[i] // sum main add kardo current index ka value 
        }
-       if(sum>maxSum){
-         maxSum = sum
+       if(sum>maxSum){  // sum ka value maXsum ka value se bada hai kya nahi check 
+         maxSum = sum // agar bada hai to sum ka value max sum main  rakhdo
        }
     
     }
-   return maxSum
+   return maxSum // return kardo  maxSum ko
 }
 
-let nums = [-2,1,-3,4,-1,2,1,-5,4];
+let nums = [-2,1,-3,4,-1,2,1,-5,4]; // array set example 
 
-console.log(maxSubArray(nums));
+console.log(maxSubArray(nums)); // print ans

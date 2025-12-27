1e3243b8e945038f05a9bc9b61ede6a32b9c0c62
Suman_Kar <www.skar2022@gmail.com>
2025-12-27 14:11:10 +0530

    Update mostWaterCOntainer.js

--- AdvanceedArray/mostWaterCOntainer.js
+++ AdvanceedArray/mostWaterCOntainer.js
@@ -3,7 +3,12 @@
 
 // question :=>  Ek Container hai usme bohot sara gight ka index hai tumko find karna hoga ki max kitna water contain kar sakte hai
 
-// ans :=> ham pehele ek ans value initialize kar 
+// ans :=> ham pehele ek ans = 0 value initialize kar lenge then do pinter i and j ko initialize kar lenge joki i start hoga index 0 se and j start hoga length-1 index matlab last se 
+// then ek loop chalega jisme  base casw hai i and j saman hai kya agar nahi hai then ans ka value ko update kar do
+// max value nikalo current ans or the i and j pointed value maise min * j-1 index main se max konsa hai
+
+// then check agar kiska height chota hai usko kardo ++
+// then return kardo ans ko
 
 function MostWaterContainer(arr){
     let ans = 0 ; i = 0 , j = arr.length-1
